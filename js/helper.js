76ac965026a450447bffeb4db6368e2589a9b244
rscabrera <rob@roberts-mbp.softlayer.local>
2016-04-08 19:38:59 -0500

    completed work

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span id="role">%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -24,25 +24,25 @@ var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
-var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkStart = '<div id="work-entry-%data%" class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
-var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectStart = '<div id="project-entry-%data%" class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
-var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolStart = '<div id="school-entry-%data%" class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
@@ -50,6 +50,7 @@ var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineStart = '<div id="online-entry-%data%" class="education-entry"></div>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
@@ -64,8 +65,8 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
+    var iName = inName(name) || function(){};
+    $('#name').html(iName);
   });
 });
 
@@ -85,7 +86,7 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  logClicks(loc.pageX, loc.pageY);
 });
 
 
@@ -109,10 +110,8 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
-  */
+  // This next line makes `map` a new Google Map JavaScript Object and attaches it to
+  // <div id="map">, which is appended as part of an exercise late in the course.
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
@@ -129,20 +128,16 @@ function initializeMap() {
     locations.push(bio.contacts.location);
 
     // iterates through school locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
-      locations.push(school.location);
-    });
+    // the locations array
+    for (var school in education.schools) {
+      locations.push(education.schools[school].location);
+    }
 
     // iterates through work locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
-      locations.push(job.location);
-    });
+    // the locations array
+    for (var job in work.jobs) {
+      locations.push(work.jobs[job].location);
+    }
 
     return locations;
   }
@@ -171,12 +166,12 @@ function initializeMap() {
     // or hover over a pin on a map. They usually contain more information
     // about a location.
     var infoWindow = new google.maps.InfoWindow({
-      content: name
+      content: '<h4>'+name+'</h4><p>I was here!</p>'
     });
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -209,16 +204,17 @@ function initializeMap() {
     var service = new google.maps.places.PlacesService(map);
 
     // Iterates through the array of locations, creates a search object for each location
-      locations.forEach(function(place){
+    for (var place in locations) {
+
       // the search request object
       var request = {
-        query: place
+        query: locations[place]
       };
 
       // Actually searches the Google Maps API for location data and runs the callback
       // function with the search results after each search.
       service.textSearch(request, callback);
-    });
+    }
   }
 
   // Sets the boundaries of the map based on pin locations
@@ -238,11 +234,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+  // Make sure the map bounds get updated on page resize
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,69 +1,177 @@
+var appendObjectProperties = function(object, location, formatter, propertyPlaceholder, valuePlaceholder) {
+  for (var property in object) {
+    var value = object[property];
+    var data = formatter.replace(propertyPlaceholder, property)
+                        .replace(valuePlaceholder, value);
+    $(location).append(data);
+  }
+};
+
+var appendListOfObjects = function(list, location, formatter, valuePlaceholder) {
+  for (var key in list) {
+    $(location).append(formatter.replace(valuePlaceholder, list[key]));
+  }
+};
+
+var replaceData = function(data, formatter, dataPlaceholder) {
+  dataPlaceholder = dataPlaceholder || '%data%';
+  return formatter.replace(dataPlaceholder, data);
+};
+
 var bio = {
-  "name": "Robert Cabrera",
-  "role": "Mangaer, IBM Cloud",
-  "contacts": {
-    "mobile": "281-220-7992",
-    "email": "rscabrera@gmail.com",
-    "github": "rscabrera",
-    "twitter": "@rs_cabrera",
-    "location": "Houston"
+  'name': 'Robert Cabrera',
+  'role': 'Manager, IBM Cloud',
+  'contacts': {
+    'mobile': "<a href='tel:5555555555'>555.555.5555</a>",
+    'email': "<a href='mailto:rscabrera@gmail.com'>rscabrera@gmail.com</a>",
+    // 'linkedin': "<a href='http://bit.ly/1N09KFX'>Robert Cabrera</a>",
+    'github': "<a href='http://bit.ly/1qBLErD'>rscabrera</a>",
+    'twitter': "<a href='http://bit.ly/1qd8Yv3'>@rs_cabrera</a>",
+    'location': "<a href='#'>Houston, TX</a>"
   },
-  "welcomeMessage": "lorem ipsum dolor sit amet etc etc etc.",
-  "skills": [
-    "awesomeness", "delivering things", "cryogenic sleep", "saving the universe"
-  ],
-  "bioPic": "https://scontent-ord1-1.xx.fbcdn.net/hphotos-xfl1/v/t1.0-9/12809683_10208362709446658_6122974549796379685_n.jpg?oh=6985c655ba93ebe6f8537fd9a90b4add&oe=57766129"
-}
+  'welcomeMessage': 'Experience leading inovative minds in engineering and the best support technicians in the world.',
+  'skills': ['Enterprise Servers', 'System Administration', 'Virtualization',
+              'SaaS', 'IaaS', 'HTML5/CSS3', 'JavaScript',
+              'Python', 'LAMP', 'MEAN', 'Nginx', 'PaaS', 'Linux',
+              'GitHub', 'AutoCad', 'Unix', 'Windows Server', 'MongoDB', 'Leadership',
+              'Project Management', 'Dev-Ops'],
+  'biopic': 'http://bit.ly/1YjXH75',
+  'display': function() {
+    var name = replaceData(bio.name, HTMLheaderName);
+    var role = replaceData(bio.role, HTMLheaderRole).replace('<hr/>','');
+
+    $('#header').prepend(role)
+                .prepend(name);
+
+    appendObjectProperties(bio.contacts, '#topContacts', HTMLcontactGeneric, '%contact%', '%data%');
+    $('#topContacts').children().clone().appendTo('#footerContacts');
+
+    $('#header').append(replaceData(bio.biopic, HTMLbioPic))
+                .append(replaceData(bio.welcomeMessage, HTMLWelcomeMsg))
+                .append(HTMLskillsStart);
+
+    appendListOfObjects(bio.skills, '#skills', HTMLskills, '%data%');
+  }
+};
 
 var education = {
-  "schools": [
-    {
-      "name": "US Army",
-      "city": "Fort Belviour, VA",
-      "degree": "BS",
-      "majors": ["Surveying"],
-      "dates": 1997
-      "url": "http://example.com"
-    },
-    {
-      "name": "WGU",
-      "city": "Salt Lake City, UT",
-      "degree": "BS",
-      "majors": ["CS"],
-      "dates": "Present",
-      "url": "http://example.com"
+  'schools': [
+    {'name': 'Defense Mapping Agency',
+     'location': 'Fort Belviour, VA',
+     'degree': 'Bachelor of Science',
+     'majors': ['Surveying'],
+     'dates': 1998,
+     'url': 'http://1.usa.gov/1SVHcg7'
+	 },
+		{'name': 'Western Governors University',
+		 'location': 'Salt Lake City, UT',
+		 'degree': 'Bachelor of Science',
+		 'majors': ['Computer Science'],
+		 'dates': 2016,
+		 'url': 'http://www.wgu.edu'
+		}
+  ],
+  'onlineCourses': [
+    {'title': 'Front-End Nanodegree',
+     'school': 'Udacity',
+     'date': 2016,
+     'url': 'http://www.udacity.com'
     }
   ],
-  "onlineCourses": [
-    {
-    "title": "Front End Development Certification",
-    "school": "Free Code Camp"
-    "dates": "Present",
-    "url": "http://freecodecamp.com"
+  'display': function() {
+    for (var i in this.schools) {
+      $('#education').append(replaceData(i, HTMLschoolStart));
+      var id = '#school-entry-' + i;
+      var school = this.schools[i];
+      $(id).append((replaceData(school.name, HTMLschoolName) + replaceData(school.degree, HTMLschoolDegree))
+           .replace('#', school.url))
+           .append(replaceData(school.dates, HTMLschoolDates))
+           .append(replaceData(school.location, HTMLschoolLocation))
+           .append(replaceData(school.majors, HTMLschoolMajor));
+    }
+
+    $('#education').append(HTMLonlineClasses);
+    for (var j in this.onlineCourses) {
+      $('#education').append(replaceData(i, HTMLonlineStart));
+      var id = '#online-entry-' + j;
+      var online = this.onlineCourses[j];
+      $(id).append(replaceData(online.title, HTMLonlineTitle) + replaceData(online.school, HTMLonlineSchool))
+           .append(replaceData(online.date, HTMLonlineDates))
+           .append(replaceData(online.url, HTMLonlineURL).replace('#', online.url));
+    }
   }
-  ]
-}
+};
 
 var work = {
-  "jobs": [
-    {
-      "employer": "IBM",
-      "title": "Manager, IBM Cloud",
-      "dates": "January 2010 - Present",
-      "description": "Resolve system administrative and web developer-type issues on various distrobutions of Linux as well as Windows Server OSes.  Database administration of MySQL, MSSQL, PostgreSQL, MongoDB, NoSQL, etc.  Network troubleshooting and administration of network equipment such as hardware firewalls (Fortigate), network gateways (Vyatta), switches (Arista/Citrix), routers (Arista/Citrix), load balancers (Citrix NetScaler), and other various networking tasks such as VLAN NAT routing or VLAN trunking.  Configure and troubleshoot various VPNs such as PPTP, SSL, or IPSec connections.  Troubleshoot and resolve various email issues ranging from routing of emails, to DNS issues, to digging through root cause analysis of SPAM emails.  Provide RCA for servers that have gone down, or are running sluggish, as well as provide investigative analysis for a hacked or rooted server.  Troubleshoot and configure various virtulization setups such as Hyper-V, VMware, XenCenter for customer and configure storage for the devices such as SAN, NAS, or iSCSI.  Developer tasks include, but are not limited to working with node.js, PHP, Ruby, API, JavaScript, or using DevOps tools such as Bluemix, Chef, Ansible, etc. to automate tasks such as provisioning virtual machines, or push live code across multiple nodes at once for an instant change."
+  'jobs': [
+    {'employer': 'United States Army',
+     'title': 'Topographic Surveyor',
+     'location': 'Fort Bragg, NC',
+     'dates': '1998-2004',
+     'description': 'Airborne'
+    },
+    {'employer': 'IBM',
+     'title': 'Manager, IBM Cloud',
+     'location': 'Houston, TX',
+     'dates': '2010-Present',
+     'description': 'All things cloud'
     }
-  ]
-}
-if(bio.skills.length > 0) {
+  ],
+  'display': function() {
+    for (var i in this.jobs) {
+      $('#workExperience').append(replaceData(i, HTMLworkStart));
+      var id = '#work-entry-' + i;
+      var job = this.jobs[i];
+      $(id).append((replaceData(job.employer, HTMLworkEmployer) + replaceData(job.title, HTMLworkTitle))
+           .replace('href="#"', ''))
+           .append(replaceData(job.dates, HTMLworkDates))
+           .append(replaceData(job.location, HTMLworkLocation))
+           .append(replaceData(job.description, HTMLworkDescription));
+    }
+  }
+};
 
-  $("#header").append(HTMLskillsStart);
+var projects = {
+  'projects': [
+    {'title': 'Cloud Things',
+     'dates': '2014-2015',
+     'description': 'Making the cloud and the internet possible',
+     'images': [
+       'http://lorempixel.com/400/200/sports',
+       'http://lorempixel.com/g/400/200',
+       'http://lorempixel.com/400/200'
+     ]
+    }
+  ],
+  'display': function() {
+    for (var i in this.projects) {
+      $('#projects').append(replaceData(i, HTMLprojectStart));
+      var id = '#project-entry-' + i;
+      var project = this.projects[i];
+      $(id).append(replaceData(project.title, HTMLprojectTitle))
+           .append(replaceData(project.dates, HTMLprojectDates))
+           .append(replaceData(project.description, HTMLprojectDescription));
+      for (var j in project.images) {
+        $(id).append(replaceData(project.images[j], HTMLprojectImage));
+      }
+    }
+  }
+};
 
-  var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-  $("#skills").append(formattedSkill);
-  formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-  $("#skills").append(formattedSkill);
-  formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-  $("#skills").append(formattedSkill);
-  formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-  $("#skills").append(formattedSkill);
+bio.display();
+education.display();
+work.display();
+projects.display();
+
+$('#mapDiv').append(googleMap);
+
+
+// $('#main').append(internationalizeButton);
+function inName(name) {
+  name = name.trim().split(" ");
+  console.log(name);
+  name[1] = name[1].toUpperCase();
+  name[0] = name[0].slice(0,1).toUpperCase() +
+    name[0].slice(1).toLowerCase();
+  return name[0] + " " + name[1];
 }
